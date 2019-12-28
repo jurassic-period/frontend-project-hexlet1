c39d3f95a7488831f9b7eeb0b0bffcd808f6f848
Yurii <ya.donny@gmail.com>
2019-12-28 14:20:02 +0300

    small fixes

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,13 @@ import { cons } from '@hexlet/pairs';
 import { getRandomNum, playGame } from '../index';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no"';
+const minNum = 1;
+const maxNum = 101;
 
 const isEven = (num) => num % 2 === 0;
 
 const getRoundData = () => {
-  const question = getRandomNum(1, 101);
+  const question = getRandomNum(minNum, maxNum);
   const correctAnswer = isEven(question) ? 'yes' : 'no';
   return cons(question, correctAnswer);
 };
